08824381359837b91d9f190e2740ad811634a719
James Best <jim@justjibba.net>
2021-06-05 02:45:40 +0100

    Update image output directory

--- src/camera.ts
+++ src/camera.ts
@@ -19,7 +19,7 @@ export function takePhoto() {
   if (!hasMotion) return
 
   // eslint-disable-next-line node/no-path-concat
-  const filename = `${__dirname}/photos/image_${count}.jpg`
+  const filename = `~/code/photos/image_${count}.jpg`
   const args = [
     "-rot", // rotate 90 degrees
     "90",
